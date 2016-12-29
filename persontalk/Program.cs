bd1d567fb01bf21966eae7d8a14ec35362c38b3e
Chris M <cmmurr2@gmail.com>
2016-12-29 15:53:05 -0500

    Text NPC Generator
    Generates a simple NPC with a random name, occupation, age and
    personality in C#.

---
+++ persontalk/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//========================================================================================
+//persontalk: generates NPCs with unique characteristics
+//
+//Future ideas: Generate multiple NPCs simultaneously
+//              Have the NPCs randomly interact
+//              Allow the player to interact with the NPCs
+namespace persontalk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInputStr = null;
+            int userInput = 0;
+
+            //Introduction
+            Console.WriteLine("==========");
+            Console.WriteLine("PersonTalk");
+            Console.WriteLine("==========");
+            Console.WriteLine();
+
+            //Menu
+            Console.WriteLine("1: Generate New Person");
+            Console.WriteLine("2: Quit");
+
+            //Generates a person and returns to menu as long as the user doesn't select "Quit"
+            //If Quit: Program Terminates
+            while (userInput != 2)
+            {
+                //User feedback
+                userInputStr = Console.ReadLine();
+
+                //Check to make sure that the value of userInputStr is either 1 or 2
+                if (userInputStr == "1" || userInputStr == "2")
+                {
+                    //Convert user's input to int
+                    userInput = Convert.ToInt32(userInputStr);
+                    Console.WriteLine();
+
+                    //Create a new random NPC
+                    Console.WriteLine("Generating a new person...");
+                    person person1 = new person();
+                    person1.initialize();
+                    Console.WriteLine();
+
+                    //Print out the NPC's details
+                    Console.WriteLine("Name: " + person1.Name);
+                    Console.WriteLine("Age: " + person1.Age);
+                    Console.WriteLine("Occupation: " + person1.Occupation);
+                    Console.WriteLine("Personality: " + person1.Personality);
+                }
+
+                //If user enters anything other than 1 or 2, show an error message
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Invalid Entry. Try again.");
+                }
+
+                //Menu
+                Console.WriteLine();
+                Console.WriteLine("1: Generate New Person");
+                Console.WriteLine("2: Quit");
+            }
+        }
+    }
+}
---
+++ persontalk/person.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Reflection;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace persontalk
+{
+    class person
+    {
+        private string npcName = "Not Specified";
+        private int npcAge = 0;
+        private string npcOccupation = "Not Specified";
+        private string npcPersonality = "Not Specified";
+        private string npcTemperment = "Not Specified";
+
+        //========================================================================================
+        //PROPERTY GET AND SET
+        //
+        //NAME get and set
+        public string Name
+        {
+            get
+            {
+                return npcName;
+            }
+
+            set
+            {
+                npcName = value;
+            }
+        }
+
+        //AGE get and set
+        public int Age
+        {
+            get
+            {
+                return npcAge;
+            }
+
+            set
+            {
+                npcAge = value;
+            }
+        }
+
+        //OCCUPATION get and set
+        public string Occupation
+        {
+            get
+            {
+                return npcOccupation;
+            }
+
+            set
+            {
+                npcOccupation = value;
+            }
+        }
+
+        //PERSONALITY get and set
+        public string Personality
+        {
+            get
+            {
+                return npcPersonality;
+            }
+
+            set
+            {
+                npcPersonality = value;
+            }
+        }
+
+        //========================================================================================
+        //RandomFromList: Takes filename of embedded resource and returns a random line from within
+        //EX: Returns a random name to be assigned to an NPC
+        private string RandomFromList(string fileName)
+        {
+            Assembly _assembly;
+            StreamReader _textStreamReader;
+
+            try
+            {
+                //open communication with specified text file
+                _assembly = Assembly.GetExecutingAssembly();
+                _textStreamReader = new StreamReader(_assembly.GetManifestResourceStream(fileName));
+                //choose a random item from the list
+                string chosen = null;
+                string line = null;
+                int numberSeen = 0;
+                var rng = new Random();
+                while ((line = _textStreamReader.ReadLine()) != null)
+                {
+                    if (rng.Next(++numberSeen) == 0)
+                    {
+                        chosen = line;
+                    }
+                }
+
+                //return the chosen item
+                return chosen;
+            }
+
+            //return an error if file can't be accessed
+            catch
+            {
+                return "error";
+            }
+        }
+
+        //========================================================================================
+        //Initialize: Generates a new NPC with a random name, age, occupation and personality
+        public void initialize()
+        {
+            var rng = new Random();
+            this.Name = RandomFromList("persontalk.names.txt");
+            this.Age = rng.Next(14, 106);
+            this.Occupation = RandomFromList("persontalk.occupations.txt");
+            this.Personality = RandomFromList("persontalk.personalities.txt");
+
+        }
+    }
+}
